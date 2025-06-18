87ac0bca7c525af7419157c72c99f49610711d7c
KamaHayashi <38317348+KamaHayashi@users.noreply.github.com>
2025-06-18 18:17:06 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // apply to all routes
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL"
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *"
+          }
+        ]
+      }
+    ];
+  }
+};
