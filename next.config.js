a73b8402b60b519b589332998a06ff3f40250399
KamaHayashi <38317348+KamaHayashi@users.noreply.github.com>
2025-06-18 18:27:00 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/:path*',
         headers: [
-          {
-            key: "X-Frame-Options",
-            value: "ALLOWALL"
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors *"
-          }
-        ]
-      }
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          { key: 'Content-Security-Policy', value: 'frame-ancestors *' }
+        ],
+      },
     ];
-  }
+  },
 };
-
